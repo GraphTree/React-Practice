58d3c93f9eac1c00f57e92e8ac621de9fc544d74
GraphTree <graphnodesed@gmail.com>
2022-07-18 14:33:20 +0900

    todo practice start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [toDo, setToDo] = useState("");
+
+  const onChange = (e) => {
+    setToDo(e.target.value);}
+
+    console.log(toDo);
+
+  return(
+  <div>
+    <input value={toDo} onChange={onChange} type = "text" placeholder="write your todo"/>
+  </div>)  ;
 }
 
 export default App;
