3ea14554a6a5b26da85f4c8b6f6a0910a7eebaef
GraphTree <graphnodesed@gmail.com>
2022-07-18 15:27:38 +0900

    deleted app.js code related todo app to start a new app, which is coin tracker

--- src/App.js
+++ src/App.js
@@ -2,38 +2,10 @@
 import {useState, useEffect} from "react"
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo ===""){
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]); 
-    console.log(toDos);
-  }
-
-  const onChange = (e) => {
-    setToDo(e.target.value);}
-
-
 
   return(
   <div>
-    <h1>My toDOs {toDos.length}</h1>
-    <form onSubmit={onSubmit}>
-      <input 
-        value={toDo} 
-        onChange={onChange} 
-        type = "text" 
-        placeholder="write your todo"/>
-        <button >Add To Do</button>
-    </form>
-    <ul>
-    {toDos.map((item, index)=> <li key={index}>{item}</li>)}
-    </ul>
+
   </div>)   ;
 }
 
