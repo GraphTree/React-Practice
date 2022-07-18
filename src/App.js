f22f75bfdeed7e1a637a6cf5a6c37c81b9ad86a7
GraphTree <graphnodesed@gmail.com>
2022-07-18 15:24:17 +0900

    deleted file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@ import {useState, useEffect} from "react"
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo ===""){
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]); 
+    console.log(toDos);
+  }
 
   const onChange = (e) => {
     setToDo(e.target.value);}
 
-    console.log(toDo);
+
 
   return(
   <div>
-    <input value={toDo} onChange={onChange} type = "text" placeholder="write your todo"/>
+    <h1>My toDOs {toDos.length}</h1>
+    <form onSubmit={onSubmit}>
+      <input 
+        value={toDo} 
+        onChange={onChange} 
+        type = "text" 
+        placeholder="write your todo"/>
+        <button >Add To Do</button>
+    </form>
+    <ul>
+    {toDos.map((item)=> <li>{item}</li>)}
+    </ul>
   </div>)  ;
 }
 
