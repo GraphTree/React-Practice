f09fcf591fdcb7289a326127b0f16486c8e3af7f
GraphTree <graphnodesed@gmail.com>
2022-07-21 14:52:43 +0900

    deleted all the file to start a new project

--- src/App.js
+++ src/App.js
@@ -4,22 +4,12 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import Home from "./routes/Home"
-import Detail from "./routes/Detail"
-import Graph from "./routes/Graph"
-
 
 function App() {
 
 return (
   <div>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="Detail" element={<Detail/>} />
-        <Route path="Graph" element={<Graph/>} />
-      </Routes>
-    </Router>
+
       
   </div>
 )
