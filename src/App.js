9cede7dd1149fe48e24f7d87d5490ea8ea502003
GraphTree <graphnodesed@gmail.com>
2022-07-25 18:58:31 +0900

    practice styled component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-// movie app
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import styled, {keyframes} from "styled-components"
 
-import {useState} from "react"
 
+const Wrapper= styled.div`
+  display : flex;`;
+
+const animation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    transform:rotate(360deg);
+    border-radius:100px;
+  }
+
+  100% {
+    transform:rotate(0deg);
+    border-radius:0px;
+`;
+
+const Box = styled.div`
+  height : 100px;
+  width : 100px ;
+  background-color : tomato ;
+  display : flex;
+  justify-content : center ;
+  aligh-item : center;
+  animation : ${animation} 2s linear infinite;
+  span {
+    font-size : 60px;
+    &:hover {
+      font-size : 10px;
+    }
+  }
+
+  `;
+
+
+
+const Text = styled.span`color:white;`;
 
 function App() {
-
-
-
   return (
-    <div>
+    <Wrapper>
+        <Box><span>WoW</span></Box>
+    </Wrapper>
       
-    </div>
+    
   )
 
 }
