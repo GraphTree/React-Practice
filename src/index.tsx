df80a72eb26c4b4044c5a84eac5c9be3ba275f5a
GraphTree <graphnodesed@gmail.com>
2022-07-25 22:32:11 +0900

    deleted and modified some of the default file for a new app

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {ThemeProvider} from "styled-components"
+
+const darkTheme = {
+  textColor : "whitesmoke",
+  backgroundColor : "black",
+}
+
+const lightTheme ={
+  textColor : "black",
+  backgroundColor : "whitesmoke",
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme = {darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
